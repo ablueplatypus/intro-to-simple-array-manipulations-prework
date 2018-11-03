c6e3af050128ed1e71054374e7b7dba44cc38549
Matt Doering <mattdoering86@gmail.com>
2018-11-03 18:02:56 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,16 +2,16 @@ def using_push
 
 end
 
-def using_unshift
-
+def using_unshift(array)
+ array.unshift
 end
 
-def using_pop
-  
+def using_pop(array)
+  array.pop
 end
 
-def pop_with_args
-  
+def pop_with_args(array)
+  array.pop(-2)
 end
 
 def using_shift(array)
